1e4aa47a789158aa60411a85895d64e33ac2ba41
suheyla <suheyla.kalkn@gmail.com>
2020-03-15 15:26:27 -0500

    second push from freature branch

--- src/gitPackageOne/GitDay3.java
+++ src/gitPackageOne/GitDay3.java
@@ -5,6 +5,7 @@ public class GitDay3 {
 	public static void main(String[]args) {
 		System.out.println("My first changes on local master");
 		System.out.println("This is my feature branch");
+		System.out.println("My second push from local feature branch");
 		
 		
 		
